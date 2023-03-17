63006d24c559c192fab9ca98a441d2a779fad965
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-17 13:58:41 -0700

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ * 	in ascending order, seperated by a comma followed by a space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 int digit1, digit2;
+
+	 for (digit1 = 0; digit
