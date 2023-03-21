421699139372d845b7ab329a11386dbc4a889a97
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-21 16:24:37 -0700

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,4 +4,38 @@
 /**
  * print_to_98 -  prints all natural numbers from n to 98,
  * followed by a new line.
- * 
+ * @n: num input
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+
+	if (n <= 98)
+	{
+		for (n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf(", ");
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (n <= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n")
+}
