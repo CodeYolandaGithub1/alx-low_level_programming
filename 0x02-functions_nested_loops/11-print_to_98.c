563702ef9eaaaf51f9cd8ec7f176969e1339a94e
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-21 16:53:21 -0700

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,30 +11,31 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n <= 98; n++;)
+		for (n <= 98; n++);
 		{
 			if (n == 98)
 			{
 				printf("%d", n);
 				printf(", ");
+				printf("\n");
 			}
 		}
 	}
 	else if (n > 98)
 	{
-		for (; n <= 98; n--;)
+		for (; n <= 98; n--);
 		{
 			if (n == 98)
 			{
 				printf("%d", n);
 				printf(", ");
+				printf("\n");
 			}
 		}
 	}
 	else
 	{
-		printf("%d, ", n)
+		printf("%d, ", n);
 		printf("98");
 	}
-	printf("\n");
 }
