e19afdcba238963a9dc402124a2621bf642f7bd4
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-21 20:41:16 -0700

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 -  prints all natural numbers from n to 98,
+ * print_to_98 -  prints all natural numbers from n to 98, < 98 >,
  * followed by a new line.
  * @n: print from this number
  * Return: Always 0 (Success)
@@ -18,8 +18,12 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 	printf("%d", n);
-	printf(", ");
 	printf("\n");
+
+	else
+	{
+		printf("%d, ", n);
+	}
 	}
 	}
 	}
@@ -30,13 +34,13 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 	printf("%d", n);
-	printf(", ");
 	printf("\n");
-	}
-	}
-	}
+
 	else
 	{
-	printf("%d, ", n);
+		printf("%d, ", n);
+	}
+	}
+	}
 	}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,10 +3,35 @@
 /**
  * times_table - a function that prints the 9 times table,
  * starting with 0
- * Return:
+ * Return: empty output
  *
  */
 
 void times_table(void)
 {
-	int 
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = (y * x);
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			if (z < 10 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0')
+			} else if (z >= 10)
+			}
+		}
+	}
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+		_putchar('\n');
+}
