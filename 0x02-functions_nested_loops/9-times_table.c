eb6f21244373bbbe9f510bb89ac2902d83fbe272
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-22 19:05:35 -0700

    Functionss

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,8 +26,11 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(z + '0')
-			} else if (z >= 10)
+				_putchar(z + '0');
+			}
+		       
+			else if (z >= 10)
+			
 			{
 				_putchar(',');
 				_putchar(' ');
