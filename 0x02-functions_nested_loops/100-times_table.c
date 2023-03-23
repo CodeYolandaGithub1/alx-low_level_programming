8bdaca40f816108769b61537e38f5960cc4548c6
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-22 20:30:45 -0700

    Functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,5 +8,46 @@
 
 void print_times_table(int n)
 {
-	return (0);
+	int x, y, z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; i++)
+		{
+			for (y = 0; y <= n; j++)
+			{
+				z = y * x;
+
+				if (y == 0)
+				{
+					_putchar(z + '0');
+				}
+				else if (z < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
