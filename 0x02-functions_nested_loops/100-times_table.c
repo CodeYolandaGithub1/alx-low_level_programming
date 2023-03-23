267cb564fd27be35bbdc85b2e938c7c48f30b68c
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-22 19:58:00 -0700

    Functions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table,
+ * starting with 0
+ * @n: number of times table
+ */
+
+void print_times_table(int n)
+{
+	return (0);
+}
