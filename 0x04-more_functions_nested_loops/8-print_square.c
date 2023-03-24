bc736dd12c1165a00398fb5c4ee86206cd8550bb
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-03-24 13:09:42 -0700

    More functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,9 @@
  * print_square - prints a square, followed by a new line
  *
  * @size: size of the square
+ * Return: Always 0.
  */
+
 void print_square(int size)
 {
 	if (size <= 0)
@@ -23,8 +25,5 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	
 }
