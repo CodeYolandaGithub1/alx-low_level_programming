61dbef25a6468b90b7a870aa32aded07839718ac
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-04-05 14:40:42 -0700

    Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,4 +3,13 @@
 /**
  * factorial - returns the factorial of a given number.
  * @n: number to return
- * Return: 
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
