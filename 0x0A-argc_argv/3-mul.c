461e7208d2064fb98f820770d02aef4a58ba465c
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-04-10 07:55:19 -0700

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - multiplies two numbers, followed by a new line
+ * main - multiplies two numbers
+ * followed by a new line
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
@@ -16,6 +15,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 }
