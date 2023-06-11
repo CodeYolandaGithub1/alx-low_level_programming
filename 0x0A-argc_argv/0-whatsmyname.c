29c0e46a8b5bba0c3ceb0ed8305400254b12b51c
CodeYolandaGithub1 <yolamaph@gmail.com>
2023-06-11 08:57:21 -0700

    argv argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: num of argument
+ * @argv: array with argument
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return(0);
+}
